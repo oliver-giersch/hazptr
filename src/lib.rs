ef13c4da4debf90bd1b630fb4dc2b43b4b55f1ac
oliver <oliver.giersch@googlemail.com>
2019-03-29 21:12:40 +0100

    adds Send impl for Guarded adds more documentation

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,10 @@
 //! might still hold references to such entries and could consequently access already freed memory.
 //!
 //! Concurrent memory reclamation schemes solve that problem by extending the lifetime of removed
-//! entries for a certain **grace period**.
+//! entries for a certain *grace period*.
 //! After this period it must be impossible for other threads to have any references to these
 //! entries anymore and they can be finally deallocated.
-//! This is similar to the concept of **Garbage Collection** in languages like Go and Java, but with
+//! This is similar to the concept of *Garbage Collection* in languages like Go and Java, but with
 //! a much more limited scope.
 //!
 //! The Hazard-pointer reclamation scheme was described by Maged M. Michael in 2004 [[1]].
@@ -23,29 +23,39 @@
 //!
 //! The API of this library follows the abstract interface defined by the [`reclaim`][reclaim]
 //! crate.
+//! Hence, it uses the following types for atomically reading and writing from and to shared memory:
+//!
+//! - [`Atomic`][Atomic]
+//! - [`Owned`][Owned]
+//! - [`Shared`][Shared]
+//! - [`Unlinked`][Unlinked]
+//! - [`Unprotected`][Unprotected]
 //!
 //! The primary type exposed by this API is [`Atomic`][Atomic], which is a shared atomic pointer
 //! with similar semantics to `Option<Box<T>>`.
 //! It provides all operations that are also supported by `AtomicPtr`, such as `store`, `load` or
 //! `compare_exchange`.
-//! All load operations on an [`Atomic`][Atomic] return (optional) [`Shared`][Shared] references.
+//! All *load* operations on an [`Atomic`][Atomic] return (optional) [`Shared`][Shared] references.
 //! [`Shared`][Shared] is a non-nullable pointer type that is protected by a hazard pointer and has
 //! similar semantics to `&T`.
 //! *Read-Modify-Write* operations (`swap`, `compare_exchange`, `compare_exchange_weak`) return
-//! [`Unlinked`][Unlinked] values. Only values that are successfully unlinked in this manner can be
-//! retired, which means they will be automatically reclaimed at some some point when it is safe to
-//! do so.
+//! [`Unlinked`][Unlinked] values if they succeed.
+//! Only values that are successfully unlinked in this manner can be retired, which means they will
+//! be automatically reclaimed at some some point when it is safe to do so.
+//! [`Unprotected`][Unprotected] is useful for comparing and storing values, which do not need to be
+//! dereferenced and hence don't need to be protected by hazard pointers.
 //!
 //! # Compare-and-Swap
 //!
-//! The atomic [`compare_exchange`][compare_exchange] (compare-and-swap) method of the [`Atomic`][Atomic]
-//! type is highly versatile and uses generics and (internal) traits in order to achieve some sense
-//! of argument *overloading*.
+//! The atomic [`compare_exchange`][compare_exchange] method of the [`Atomic`][Atomic] type is
+//! highly versatile and uses generics and (internal) traits in order to achieve some degree of
+//! argument *overloading*.
 //! The `current` and `new` arguments accept a wide variety of pointer types interchangeably.
-//! For instance, `current` accepts values of types like [`Shared`][Shared] or `Option<Shared>`.
+//!
+//! For instance, `current` accepts values of both types [`Shared`][Shared] and `Option<Shared>`.
 //! It also accepts [`Unprotected`][Unprotected] or `Option<Unprotected>` values.
-//! A compare-and-swap can only succeed if the `current` value equals the value that is actually
-//! stored in the [`Atomic`][Atomic].
+//! A *compare-and-swap*  can only succeed if the `current` value is equal to the value that is
+//! actually stored in the [`Atomic`][Atomic].
 //! Consequently, the return type of this method adapts to the input type:
 //! When `current` is either a [`Shared`][Shared] or an [`Unprotected`][Unprotected], the return
 //! type is [`Unlinked`][Unlinked], since all of these types are non-nullable.
@@ -55,12 +65,30 @@
 //! [`Unprotected`][Unprotected] or `Option` thereof.
 //! Care has to be taken when inserting a [`Shared`][Shared] in this way, as it is possible to
 //! insert the value twice at different positions of the same collection, which violates the primary
-//! reclamation invariant (which is also the reason why `retire` is unsafe): It must be
-//! impossible for a thread to read a reference to a value that has previously been retired.
+//! reclamation invariant (which is also the reason why `retire` is unsafe):
+//! It must be impossible for a thread to read a reference to a value that has previously been
+//! retired.
+//!
+//! When a *compare-and-swap* fails, a `struct` is returned that contains both the *actual* value
+//! and the value that was attempted to be inserted.
+//! This ensures that move-only types like [`Owned`][Owned] and [`Unlinked`][Unlinked] can be
+//! retrieved again in the case of a failed *compare-and-swap*.
+//!
+//! The other methods of [`Atomic`][Atomic] are similarly versatile in terms of accepted argument
+//! types.
 //!
 //! # Pointer Tagging
 //!
-//! ...
+//! Many concurrent algorithms require the use of atomic pointers with additional information stored
+//! in one or more of a pointer's lower bits.
+//! For this purpose the [`reclaim`][reclaim] crate provides a type-based generic solution for
+//! making pointer types markable.
+//! The number of usable lower bits is part of the type signature of types like [`Atomic`][Atomic]
+//! or [`Owned`][Owned].
+//! If the pointed-to type is not able to provide the required number of mark bits (which depends on
+//! its alignment) this will lead to a compilation error.
+//! Since the number of mark bits is part of the types themselves, using zero mark bits also has
+//! zero runtime overhead.
 //!
 //! [1]: https://dl.acm.org/citation.cfm?id=987595
 //! [reclaim]: https://github.com/oliver-giersch/reclaim
@@ -81,7 +109,7 @@ use reclaim::{MarkedNonNull, MarkedPtr, NotEqual, Protect, Reclaim, Unsigned};
 pub type Atomic<T, N> = reclaim::Atomic<T, N, HP>;
 /// Shared reference to a value loaded from shared memory that is protected by a hazard pointer.
 pub type Shared<'g, T, N> = reclaim::Shared<'g, T, N, HP>;
-/// TODO: Doc...
+/// A pointer type for heap allocation like `Box` that can be marked.
 pub type Owned<T, N> = reclaim::Owned<T, N, HP>;
 /// Unique reference to a value that has been successfully unlinked and can be retired.
 pub type Unlinked<T, N> = reclaim::Unlinked<T, N, HP>;
@@ -139,17 +167,7 @@ pub struct Guarded<T, N: Unsigned> {
     hazard: Option<(HazardPtr, MarkedNonNull<T, N>)>,
 }
 
-impl<T, N: Unsigned> Guarded<T, N> {
-    /// Takes the internally stored hazard pointer, sets it to protect the given pointer (`protect`)
-    /// and wraps it in a `HazardPtr`.
-    #[inline]
-    fn take_hazard_and_protect(&mut self, protect: NonNull<()>) -> Option<HazardPtr> {
-        self.hazard.take().map(|(handle, _)| {
-            handle.set_protected(protect);
-            handle
-        })
-    }
-}
+unsafe impl<T, N: Unsigned> Send for Guarded<T, N> {}
 
 impl<T, N: Unsigned> Protect for Guarded<T, N> {
     type Item = T;
@@ -247,6 +265,18 @@ impl<T, N: Unsigned> Protect for Guarded<T, N> {
 
         // if `hazard` is Some(_) the contained `HazardPtr` is dropped
         self.hazard = None;
+    }
+}
+
+impl<T, N: Unsigned> Guarded<T, N> {
+    /// Takes the internally stored hazard pointer, sets it to protect the given pointer (`protect`)
+    /// and wraps it in a `HazardPtr`.
+    #[inline]
+    fn take_hazard_and_protect(&mut self, protect: NonNull<()>) -> Option<HazardPtr> {
+        self.hazard.take().map(|(handle, _)| {
+            handle.set_protected(protect);
+            handle
+        })
     }
 }
 
