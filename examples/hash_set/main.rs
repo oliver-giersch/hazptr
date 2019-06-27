83dd8f7fbdbe9bb8c07abaa69ef5638f2d3f8129
oliver-giersch <oliver.giersch@googlemail.com>
2019-06-27 19:03:08 +0200

    hash_set working again, but maybe unsound?

--- examples/hash_set/main.rs
+++ examples/hash_set/main.rs
@@ -1,26 +1,30 @@
 // implementation is currently defunct
 
-fn main() {}
+mod ordered;
 
-/* use std::borrow::Borrow;
+use std::borrow::Borrow;
 use std::collections::hash_map::RandomState;
 use std::hash::{BuildHasher, Hash, Hasher};
 use std::mem;
 use std::slice;
 use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::Arc;
 use std::thread;
 
-mod ordered;
+use hazptr::reclaim;
+use hazptr::Guard;
+use reclaim::prelude::*;
 
 use crate::ordered::OrderedSet;
 
-const DEFAULT_BUCKETS: usize = 256;
+const DEFAULT_BUCKETS: usize = 64;
 
-/// A concurrent hash set.
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// HashSet
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 pub struct HashSet<T, S = RandomState> {
-    size: usize,
-    buckets: Box<[OrderedSet<T>]>,
-    hash_builder: S,
+    inner: Arc<RawHashSet<T, S>>,
 }
 
 impl<T: Ord + Hash> Default for HashSet<T, RandomState> {
@@ -57,9 +61,11 @@ where
     #[inline]
     pub fn with_hasher(hash_builder: S) -> Self {
         Self {
-            size: DEFAULT_BUCKETS,
-            buckets: Self::allocate_buckets(DEFAULT_BUCKETS),
-            hash_builder,
+            inner: Arc::new(RawHashSet {
+                size: DEFAULT_BUCKETS,
+                buckets: Self::allocate_buckets(DEFAULT_BUCKETS),
+                hash_builder,
+            }),
         }
     }
 
@@ -71,19 +77,30 @@ where
     #[inline]
     pub fn with_hasher_and_buckets(hash_builder: S, buckets: usize) -> Self {
         assert!(buckets > 0, "hash set needs at least one bucket");
-        Self { size: buckets, buckets: Self::allocate_buckets(buckets), hash_builder }
+        Self {
+            inner: Arc::new(RawHashSet {
+                size: buckets,
+                buckets: Self::allocate_buckets(buckets),
+                hash_builder,
+            }),
+        }
     }
 
     /// Returns the number of buckets in this hash set.
     #[inline]
     pub fn buckets(&self) -> usize {
-        self.size
+        self.inner.size
     }
 
     /// Returns a reference to the set's `BuildHasher`.
     #[inline]
     pub fn hasher(&self) -> &S {
-        &self.hash_builder
+        &self.inner.hash_builder
+    }
+
+    #[inline]
+    pub fn handle(&self) -> Handle<T, S> {
+        Handle { inner: Arc::clone(&self.inner), guards: Guards::new() }
     }
 
     /// Allocates a boxed slice of ordered sets.
@@ -100,7 +117,120 @@ where
             Box::from_raw(slice)
         }
     }
+}
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Handle
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+pub struct Handle<T, S = RandomState> {
+    inner: Arc<RawHashSet<T, S>>,
+    guards: Guards,
+}
+
+impl<T, S> Handle<T, S>
+where
+    T: Hash + Ord + 'static,
+    S: BuildHasher,
+{
+    /// Returns `true` if the set contains the given `value`.
+    #[inline]
+    pub fn contains<Q>(&mut self, value: &Q) -> bool
+    where
+        T: Borrow<Q>,
+        Q: Hash + Ord,
+    {
+        self.inner.contains(value, &mut self.guards)
+    }
+
+    /// Returns a reference to the value in the set, if any, that is equal to the given value.
+    ///
+    /// The value may be any borrowed form of the set's value type, but [`Hash`][Hash] and
+    /// [`Eq`][Eq] on the borrowed form *must* match those for the value type.
+    ///
+    /// [Hash]: std::hash::Hash
+    /// [Eq]: std::cmp::Eq
+    #[inline]
+    pub fn get<Q>(&mut self, value: &Q) -> Option<&T>
+    where
+        T: Borrow<Q>,
+        Q: Hash + Ord,
+    {
+        self.inner.get(value, &mut self.guards)
+    }
+
+    /// Adds a value to the set.
+    ///
+    /// If the set did not have this value present, `true` is returned.
+    /// If the set did have this value present, `false` is returned.
+    #[inline]
+    pub fn insert(&mut self, value: T) -> bool {
+        self.inner.insert(value, &mut self.guards)
+    }
+
+    /// Removes a value from the set. Returns whether the value was
+    /// present in the set.
+    ///
+    /// The value may be any borrowed form of the set's value type, but
+    /// [`Hash`][Hash] and [`Eq`][Eq] on the borrowed form *must* match those for
+    /// the value type.
+    ///
+    /// [Hash]: std::hash::Hash
+    /// [Eq]: std::cmp::Eq
+    #[inline]
+    pub fn remove<Q>(&mut self, value: &Q) -> bool
+    where
+        T: Borrow<Q>,
+        Q: Ord + Hash,
+    {
+        self.inner.remove(value, &mut self.guards)
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Guards
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/// A container for the three hazard pointers required to safely traverse a hash
+/// set.
+#[derive(Debug, Default)]
+struct Guards {
+    prev: Guard,
+    curr: Guard,
+    next: Guard,
+}
+
+impl Guards {
+    #[inline]
+    fn new() -> Self {
+        Self { prev: Guard::new(), curr: Guard::new(), next: Guard::new() }
+    }
+
+    /// Releases all contained guards.
+    #[inline]
+    fn release_all(&mut self) {
+        self.prev.release();
+        self.curr.release();
+        self.next.release();
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// RawHashSet
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/// A concurrent hash set.
+struct RawHashSet<T, S = RandomState> {
+    size: usize,
+    buckets: Box<[OrderedSet<T>]>,
+    hash_builder: S,
+}
+
+impl<T, S> RawHashSet<T, S>
+where
+    T: Hash + Ord,
+    S: BuildHasher,
+{
     /// Generates a hash for `value` and transforms it into a slice index for the given number of
     /// buckets.
     #[inline]
@@ -115,21 +245,21 @@ where
     }
 }
 
-impl<T, S> HashSet<T, S>
+impl<T, S> RawHashSet<T, S>
 where
     T: Hash + Ord + 'static,
     S: BuildHasher,
 {
     /// Returns `true` if the set contains the given `value`.
     #[inline]
-    pub fn contains<Q>(&self, value: &Q) -> bool
+    pub fn contains<Q>(&self, value: &Q, guards: &mut Guards) -> bool
     where
         T: Borrow<Q>,
         Q: Hash + Ord,
     {
         let set = &self.buckets[Self::make_hash(&self.hash_builder, value, self.size)];
-        let res = set.get(value).is_some();
-        //guards.release_all();
+        let res = set.get(value, guards).is_some();
+        guards.release_all();
 
         res
     }
@@ -142,13 +272,13 @@ where
     /// [Hash]: std::hash::Hash
     /// [Eq]: std::cmp::Eq
     #[inline]
-    pub fn get<'g, Q>(&self, value: &Q) -> Option<&'g T>
+    pub fn get<'g, Q>(&self, value: &Q, guards: &'g mut Guards) -> Option<&'g T>
     where
         T: Borrow<Q>,
         Q: Hash + Ord,
     {
         let set = &self.buckets[Self::make_hash(&self.hash_builder, value, self.size)];
-        set.get(value)
+        set.get(value, guards)
     }
 
     /// Adds a value to the set.
@@ -156,9 +286,9 @@ where
     /// If the set did not have this value present, `true` is returned.
     /// If the set did have this value present, `false` is returned.
     #[inline]
-    pub fn insert(&self, value: T) -> bool {
+    pub fn insert(&self, value: T, guards: &mut Guards) -> bool {
         let set = &self.buckets[Self::make_hash(&self.hash_builder, &value, self.size)];
-        set.insert_node(value)
+        set.insert_node(value, guards)
     }
 
     /// Removes a value from the set. Returns whether the value was
@@ -171,13 +301,13 @@ where
     /// [Hash]: std::hash::Hash
     /// [Eq]: std::cmp::Eq
     #[inline]
-    pub fn remove<Q>(&self, value: &Q) -> bool
+    pub fn remove<Q>(&self, value: &Q, guards: &mut Guards) -> bool
     where
         T: Borrow<Q>,
         Q: Ord + Hash,
     {
         let set = &self.buckets[Self::make_hash(&self.hash_builder, value, self.size)];
-        set.remove_node(value)
+        set.remove_node(value, guards)
     }
 }
 
@@ -234,33 +364,34 @@ impl Ord for DropI8<'_> {
 
 fn test_insert_remove() {
     let set = HashSet::with_buckets(1);
+    let mut handle = set.handle();
 
     // insert
-    assert!(set.insert(0));
-    assert!(set.insert(1));
-    assert!(set.insert(-10));
-    assert!(set.insert(10));
-    assert!(set.insert(5));
-    assert!(set.insert(-5));
-    assert!(set.insert(7));
-    assert!(set.insert(-2));
+    assert!(handle.insert(0));
+    assert!(handle.insert(1));
+    assert!(handle.insert(-10));
+    assert!(handle.insert(10));
+    assert!(handle.insert(5));
+    assert!(handle.insert(-5));
+    assert!(handle.insert(7));
+    assert!(handle.insert(-2));
 
     // remove
-    assert!(set.remove(&-10));
-    assert!(set.remove(&-5));
-    assert!(set.remove(&-2));
-    assert!(set.remove(&0));
-    assert!(set.remove(&5));
-    assert!(set.remove(&7));
-    assert!(set.remove(&10));
+    assert!(handle.remove(&-10));
+    assert!(handle.remove(&-5));
+    assert!(handle.remove(&-2));
+    assert!(handle.remove(&0));
+    assert!(handle.remove(&5));
+    assert!(handle.remove(&7));
+    assert!(handle.remove(&10));
 
-    assert!(!set.contains(&-10));
-    assert!(!set.contains(&-5));
-    assert!(!set.contains(&-2));
-    assert!(!set.contains(&0));
-    assert!(!set.contains(&5));
-    assert!(!set.contains(&7));
-    assert!(!set.contains(&10));
+    assert!(!handle.contains(&-10));
+    assert!(!handle.contains(&-5));
+    assert!(!handle.contains(&-2));
+    assert!(!handle.contains(&0));
+    assert!(!handle.contains(&5));
+    assert!(!handle.contains(&7));
+    assert!(!handle.contains(&10));
 
     println!("test_insert_remove: success");
 }
@@ -269,15 +400,16 @@ fn test_random() {
     use rand::prelude::*;
 
     let set = HashSet::with_buckets(1);
+    let mut handle = set.handle();
 
     let mut conflicts = 0;
     for _ in 0..10_000 {
         let value: i8 = rand::thread_rng().gen();
-        if set.contains(&value) {
+        if handle.contains(&value) {
             conflicts += 1;
-            set.remove(&value);
+            handle.remove(&value);
         } else {
-            set.insert(value);
+            handle.insert(value);
         }
     }
 
@@ -285,8 +417,6 @@ fn test_random() {
 }
 
 fn main() {
-    use std::sync::Arc;
-
     use rand::prelude::*;
 
     const THREADS: usize = 8;
@@ -307,10 +437,11 @@ fn main() {
     test_random();
 
     // the single bucket ensures maximum contention
-    let set = Arc::new(HashSet::with_buckets(1));
+    let set = HashSet::with_buckets(1);
+
     let handles: Vec<_> = (0..THREADS)
         .map(|id| {
-            let set = Arc::clone(&set);
+            let mut handle = set.handle();
             thread::spawn(move || {
                 let mut alloc_count = 0u32;
 
@@ -320,10 +451,10 @@ fn main() {
                     }
 
                     let value: i8 = rand::thread_rng().gen();
-                    if set.contains(&value) {
-                        set.remove(&value);
+                    if handle.contains(&value) {
+                        handle.remove(&value);
                     } else {
-                        set.insert(DropI8(value, &THREAD_COUNTS[id]));
+                        handle.insert(DropI8(value, &THREAD_COUNTS[id]));
                         alloc_count += 1;
                     }
                 }
@@ -344,4 +475,3 @@ fn main() {
     );
     println!("success, no leaks detected.");
 }
-*/
--- examples/hash_set/ordered/mod.rs
+++ examples/hash_set/ordered/mod.rs
@@ -1,32 +1,26 @@
-//mod iter;
-
 use std::borrow::Borrow;
-use std::cell::UnsafeCell;
 use std::cmp::Ordering::{Equal, Greater};
 use std::mem;
-use std::ptr::NonNull;
 use std::sync::atomic::Ordering::{Acquire, Relaxed, Release};
 
-pub type Atomic<T> = hazptr::Atomic<T, typenum::U1>;
-pub type Owned<T> = hazptr::Owned<T, typenum::U1>;
-pub type Shared<'g, T> = hazptr::Shared<'g, T, typenum::U1>;
-
 use hazptr::reclaim::align::CacheAligned;
-use hazptr::reclaim::prelude::{Marked, Protect};
+use hazptr::reclaim::prelude::*;
 use hazptr::typenum;
-use hazptr::Guard;
+use typenum::U1;
 
-//use self::iter::{InsertPos, Iter, IterPos};
+use crate::Guards;
 
 use self::FindResult::*;
 
-thread_local!(static GUARDS: UnsafeCell<Guards> = UnsafeCell::new(Guards::new()));
+pub type Atomic<T> = hazptr::Atomic<T, U1>;
+pub type Owned<T> = hazptr::Owned<T, U1>;
+pub type Shared<'g, T> = hazptr::Shared<'g, T, U1>;
 
 const DELETE_TAG: usize = 1;
 
 /// A concurrent linked-list based ordered set.
 #[derive(Debug, Default)]
-pub struct OrderedSet<T> {
+pub(crate) struct OrderedSet<T> {
     head: Atomic<Node<T>>,
 }
 
@@ -37,29 +31,14 @@ where
     /// Inserts a new node for the given `value` and returns `true`, if it did
     /// not already exist in the set.
     #[inline]
-    pub fn insert_node(&self, value: T) -> bool {
-        GUARDS.with(|cell| {
-            let guards = unsafe { &mut *cell.get() };
-
-            let mut node = Owned::new(Node::new(value));
-            let success = loop {
-                let elem = &node.elem;
-            };
-        });
-
-        unimplemented!()
-
-        /*let mut node = Owned::new(Node::new(value));
+    pub fn insert_node(&self, value: T, guards: &mut Guards) -> bool {
+        let mut node = Owned::new(Node::new(value));
 
         let success = loop {
-            let elem = &node.elem;
-            if let Ok(InsertPos { prev: pos, next }) =
-                Iter::new(&self, guards).find_insert_position(elem)
-            {
-                node.next.store(next, Relaxed);
-                // (ORD:1) this `Release` CAS synchronizes-with the `Acquire` loads (ITE:1), (ITE:2)
-                // and the `Acquire` CAS (ORD:2)
-                match pos.compare_exchange(next, node, Release, Relaxed) {
+            let elem = node.elem();
+            if let Insert { prev, next } = self.find(elem, guards) {
+                node.next().store(next, Relaxed);
+                match prev.compare_exchange(next, node, Release, Relaxed) {
                     Ok(_) => break true,
                     Err(failure) => node = failure.input,
                 }
@@ -69,25 +48,25 @@ where
         };
 
         guards.release_all();
-        success*/
+        success
     }
 
     /// Tries to remove a node containing the given `value` from the set and
     /// returns `true`, if the value was found and successfully removed.
     #[inline]
-    pub fn remove_node<Q>(&self, value: &Q) -> bool
+    pub fn remove_node<Q>(&self, value: &Q, guards: &mut Guards) -> bool
     where
         T: Borrow<Q>,
         Q: Ord,
     {
-        /*let success = loop {
-            match Iter::new(&self, guards).find_insert_position(value) {
-                Ok(_) => break false,
-                Err(IterPos { prev, curr, next }) => {
+        let success = loop {
+            match self.find(value, guards) {
+                Insert { .. } => break false,
+                Found { prev, curr, next } => {
                     let next_marked = Marked::marked(next, DELETE_TAG);
-                    // (ORD:2) this `Acquire` CAS synchronizes-with the `Release` CAS (ITE:3),
-                    // (ORD:1), (ORD:3)
-                    if curr.next.compare_exchange(next, next_marked, Acquire, Relaxed).is_err() {
+                    // (ORD:x) this `Acquire` CAS synchronizes-with the `Release` CAS (ITE:d),
+                    // (ORD:y), (ORD:a)
+                    if curr.next().compare_exchange(next, next_marked, Acquire, Relaxed).is_err() {
                         continue;
                     }
 
@@ -96,41 +75,89 @@ where
                     match prev.compare_exchange(curr, next, Release, Relaxed) {
                         Ok(unlinked) => unsafe { unlinked.retire() },
                         Err(_) => {
-                            let _ = Iter::new(&self, guards).find_insert_position(value);
+                            let _ = self.find(value, guards);
                         }
                     }
 
                     break true;
                 }
-            }
+            };
         };
 
         guards.release_all();
-        success*/
-
-        unimplemented!()
+        success
     }
 
     /// Returns a reference to the value in the set, if any, that is equal to
     /// the given `value`.
     #[inline]
-    pub fn get<'g, Q>(&self, value: &Q) -> Option<&'g T>
+    pub fn get<'g, Q>(&self, value: &Q, guards: &'g mut Guards) -> Option<&'g T>
     where
         T: Borrow<Q>,
         Q: Ord,
     {
-        GUARDS.with(|cell| {
-            let guards = unsafe { &mut *cell.get() };
-            match self.iter(guards).find_elem(value) {
-                Insert(_) => None,
-                Found(IterPos { curr, .. }) => Some(Shared::into_ref(curr).elem()),
-            }
-        })
+        match self.find(value, guards) {
+            Found { curr, .. } => Some(Shared::into_ref(curr).elem()),
+            Insert { .. } => None,
+        }
     }
 
-    #[inline]
-    fn iter<'g>(&self, guards: &'g mut Guards) -> Iter<'_, 'g, T> {
-        unimplemented!()
+    fn find<'set, 'g, Q>(&'set self, value: &Q, guards: &'g mut Guards) -> FindResult<'set, 'g, T>
+    where
+        T: Borrow<Q>,
+        Q: Ord,
+        'g: 'set,
+    {
+        'retry: loop {
+            let mut prev: &'g Atomic<Node<T>> = unsafe { &*(&self.head as *const _) };
+            while let Some(curr_marked) = prev.load(Acquire, &mut guards.curr) {
+                let (curr, curr_tag) = Shared::decompose(curr_marked);
+                if curr_tag == DELETE_TAG {
+                    continue 'retry;
+                }
+
+                let curr_next: &'g Atomic<Node<_>> = unsafe { &*(curr.next() as *const _) };
+                let next_raw = curr_next.load_raw(Relaxed);
+
+                match curr_next.load_marked_if_equal(next_raw, Acquire, &mut guards.next) {
+                    Err(_) => continue 'retry,
+                    Ok(next_marked) => {
+                        if prev.load_raw(Relaxed) != curr.as_marked_ptr() {
+                            continue 'retry;
+                        }
+
+                        let (next, next_tag) = Marked::decompose(next_marked);
+                        if next_tag == DELETE_TAG {
+                            match prev.compare_exchange(curr, next, Release, Relaxed) {
+                                Ok(unlinked) => unsafe { unlinked.retire() },
+                                Err(_) => continue 'retry,
+                            };
+                        } else {
+                            unsafe {
+                                match curr.elem().borrow().cmp(value) {
+                                    Equal => {
+                                        return Found {
+                                            prev,
+                                            curr: mem::transmute(curr),
+                                            next: mem::transmute(next),
+                                        }
+                                    }
+                                    Greater => {
+                                        return Insert { prev, next: Some(mem::transmute(curr)) }
+                                    }
+                                    _ => {}
+                                };
+                            }
+
+                            prev = curr_next;
+                            mem::swap(&mut guards.prev, &mut guards.curr);
+                        }
+                    }
+                };
+            }
+
+            return Insert { prev, next: None };
+        }
     }
 }
 
@@ -142,34 +169,6 @@ impl<T> Drop for OrderedSet<T> {
             node = curr.next.take();
             mem::drop(curr);
         }
-    }
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-// Guards
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/// A container for the three hazard pointers required to safely traverse a hash
-/// set.
-#[derive(Debug, Default)]
-struct Guards {
-    prev: Guard,
-    curr: Guard,
-    next: Guard,
-}
-
-impl Guards {
-    #[inline]
-    fn new() -> Self {
-        Self { prev: Guard::new(), curr: Guard::new(), next: Guard::new() }
-    }
-
-    /// Releases all contained guards.
-    #[inline]
-    fn release_all(&mut self) {
-        self.prev.release();
-        self.curr.release();
-        self.next.release();
     }
 }
 
@@ -201,63 +200,17 @@ impl<T> Node<T> {
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-// Iter
+// FindResult
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[derive(Debug)]
-struct Iter<'set, 'g, T> {
-    head: &'set Atomic<Node<T>>,
-    guards: &'g mut Guards,
-    prev: NonNull<Atomic<Node<T>>>,
-    next: NonNull<Atomic<Node<T>>>,
-}
-
-impl<'set, 'g, T> Iter<'set, 'g, T>
-where
-    T: 'static,
-    'g: 'set,
-{
-    fn find_elem<Q>(mut self, elem: &Q) -> FindResult<'set, 'g, T>
-    where
-        T: Borrow<Q>,
-        Q: Ord,
-    {
-        while let Some(res) = self.next() {
-            if let Ok(pos) = res {
-                let key = pos.curr.elem().borrow();
-                match key.cmp(elem) {
-                    Equal => return Found(unsafe { mem::transmute(pos) }),
-                    Greater => return Insert(InsertPos { prev: pos.prev, next: Some(pos.curr) }),
-                    _ => {}
-                }
-            }
-        }
-
-        Insert(InsertPos { prev: unsafe { &*self.prev.as_ptr() }, next: None })
-    }
-
-    fn next<'iter>(&'iter mut self) -> Option<Result<IterPos<'iter, 'iter, T>, IterErr>> {
-        unimplemented!()
-    }
-}
-
-struct IterPos<'set, 'g, T> {
-    prev: &'set Atomic<Node<T>>,
-    curr: Shared<'g, Node<T>>,
-    next: Marked<Shared<'g, Node<T>>>,
-}
-
-struct InsertPos<'set, 'g, T> {
-    prev: &'set Atomic<Node<T>>,
-    next: Option<Shared<'g, Node<T>>>,
-}
-
 enum FindResult<'set, 'g, T> {
-    Found(IterPos<'set, 'g, T>),
-    Insert(InsertPos<'set, 'g, T>),
-}
-
-enum IterErr {
-    Retry,
-    Stalled,
+    Found {
+        prev: &'set Atomic<Node<T>>,
+        curr: Shared<'g, Node<T>>,
+        next: Marked<Shared<'g, Node<T>>>,
+    },
+    Insert {
+        prev: &'set Atomic<Node<T>>,
+        next: Option<Shared<'g, Node<T>>>,
+    },
 }
