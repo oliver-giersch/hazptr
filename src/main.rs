6a1cb5223aa31500adbf38a7ac5d892bf3ce0248
oliver <oliver.giersch@googlemail.com>
2019-07-15 21:47:00 +0200

    adjusts main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 // this is mainly useful for checking the assembly output
 
-use std::sync::atomic::Ordering::Acquire;
+use std::sync::atomic::Ordering::{Acquire, Release};
 
-use hazptr::Guard;
+use hazptr::{Guard, Owned};
 
 type Atomic<T> = hazptr::Atomic<T, hazptr::typenum::U0>;
 
 static GLOBAL: Atomic<i32> = Atomic::null();
 
 fn main() {
+    init();
     let mut guard = Guard::new();
-    let _global = GLOBAL.load(Acquire, &mut guard);
+    let global = GLOBAL.load(Acquire, &mut guard).unwrap();
+}
+
+#[inline(never)]
+fn init() {
+    GLOBAL.store(Owned::new(1), Release);
 }
