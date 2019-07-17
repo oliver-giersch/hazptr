4a29e5678bb6a3d158ded59194d508efba515f53
oliver-giersch <oliver.giersch@googlemail.com>
2019-07-17 16:41:03 +0200

    adds bench

--- benches/retire.rs
+++ benches/retire.rs
@@ -12,7 +12,7 @@ type Atomic<T> = hazptr::Atomic<T, hazptr::typenum::U0>;
 type Owned<T> = hazptr::Owned<T, hazptr::typenum::U0>;
 
 #[bench]
-fn retire(b: &mut Bencher) {
+fn single_retire(b: &mut Bencher) {
     CONFIG.init_once(|| Config::with_params(128));
 
     let global = Atomic::new(1);
@@ -20,5 +20,20 @@ fn retire(b: &mut Bencher) {
     b.iter(|| {
         let unlinked = global.swap(Owned::new(1), Relaxed).unwrap();
         unsafe { unlinked.retire() };
+    });
+}
+
+#[bench]
+fn multi_retire(b: &mut Bencher) {
+    const STEPS: u32 = 100_000;
+    CONFIG.init_once(|| Config::with_params(128));
+
+    let global = Atomic::new(1);
+
+    b.iter(|| {
+        for _ in 0..STEPS {
+            let unlinked = global.swap(Owned::new(1), Relaxed).unwrap();
+            unsafe { unlinked.retire() };
+        }
     });
 }
